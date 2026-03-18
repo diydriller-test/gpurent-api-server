1c9059d41a56e65e1e15300f405dd6bb78d4c3d7
diydriller <dhrhd080@naver.com>
2026-03-18 09:54:09 +0900

    fix: cors 처리

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
@@ -13,6 +14,14 @@ app = FastAPI(
     title="GPU Rent API Server",
     description="API 서버",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router)
